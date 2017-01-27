fe8091294da83e39d4649f94f2a88712971055e3
Rui Abreu Ferreira <raf-ep@gmx.com>
2017-01-27 14:28:33 +0000

    Fix null ptr check in get_ttyname()

--- gpgagent/src/helpers.rs
+++ gpgagent/src/helpers.rs
@@ -1,12 +1,16 @@
 use std::ffi;
+use std::ptr::null_mut;
 
 extern crate libc;
 
 #[cfg(unix)]
 pub fn get_ttyname() -> Option<String> {
-    let c_str = unsafe { 
-        ffi::CStr::from_ptr(libc::ttyname(0)) 
-    };
+    let ptr = unsafe { libc::ttyname(0) };
+    if ptr == null_mut() {
+        return None;
+    }
+
+    let c_str = unsafe { ffi::CStr::from_ptr(ptr) };
     c_str.to_str()
         .ok()
         .map(|v| v.to_owned())
